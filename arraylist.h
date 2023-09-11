a390fa9b230d62c57e9ed6bd72fa2eafb80f8264
Jay Vince Serato <jayvince.serato@cit.edu>
2023-09-11 18:17:46 +0800

    Made ArrayList dynamic

--- arraylist.h
+++ arraylist.h
@@ -1,48 +1,64 @@
 #include <iostream>
+#include <cstdlib>
 #include "list.h"
 using namespace std;
 
 class ArrayList : public List {
-	int size = 0;
-	int array[5];
-	
 	public:
+	int* array = (int*) calloc( capacity , sizeof(int) );
+	int capacity = 5;
+	int size = 0;
+	
+	ArrayList() {
+		capacity = 5;
+		array = (int*) calloc( capacity , sizeof(int) );
+		cout << "Address of array: " << array << endl;
+	}
+	
 	void add(int num) {
 		cout << "Adding " << num << endl;
-		if (size < 5) {
-			array[size] = num;
-			size++;	
-		} else {
-			cout << "PUNO NA" << endl;
+		if (size >= capacity) {
+			capacity *= 1.5;
+			cout << "Resizing to " << capacity << endl;
+			array = (int*) realloc(array, capacity * sizeof(int) );
+			cout << "Address of array: " << array << endl;
 		}
+		array[size] = num;
+		size++;	
 	}
 	
 	int get(int pos) {
 		return array[pos-1];
 	}
-	
+	// capacity *= 1.5; array = (int*) realloc(array, capacity * sizeof(int) );
+	// Note that when the number of elements reach 2/3 of the capacity, 
+	// you are to reallocate the array by reducing its size by 25%, 
+	// Additionally, add the functionality to maintain a minimum 
+	// capacity of 5. Write which line number you will add your code after.
 	int remove(int num) {
 		for (int i = 0; i < size; i++) {
-			// Step 1: Find num
 			if (array[i] == num) {
-				// Step 2: Move elements to left
 				for (int j = i+1; j < size; j++) {
 					array[j-1] = array[j];
 				}
-				// Step 3: Set last to 0
 				array[size-1] = 0;
-				// Step 4: Decrement size
 				size--;
-				// Step 5: Return pos
+				if (size <= (2/3.0) * capacity) {
+					capacity *= 0.75;
+					if (capacity < 5) {
+						capacity = 5;
+					}
+					array = (int*) realloc(array, capacity * sizeof(int) );
+				}
 				return i+1;
 			} 
 		}
 		return -1;
 	}
-	
+	// TIME REMAINING: 0:00 MINUTE 
 	void print() {
 		cout << "Size: " << size << endl;
-		for (int i = 0; i < size; i++) {
+		for (int i = 0; i < capacity; i++) {
 			cout << array[i] << " ";
 		}
 		cout << endl;
--- main.cpp
+++ main.cpp
@@ -2,7 +2,29 @@
 #include "arraylist.h"
 using namespace std;
 int main() {
+	int *array = (int*) malloc(5 * sizeof(int));
+	cout << "Array is " << array << endl;
+	int* new_array = (int*) realloc(array, 10*sizeof(int));
+	
 	List* list = new ArrayList();
+	int n;
+	char op;
+	do {
+		cin >> op;
+		switch(op) {
+			case 'a':
+				cin >> n;
+				list->add(n);
+				break;
+			case 'p':
+				list->print();
+				break;
+			case 'r':
+				cin >> n;
+				list->remove(n);
+				break;
+		}
+	} while (op != 'x');
 	list->add(13);
 	list->add(7);
 	list->add(20);
